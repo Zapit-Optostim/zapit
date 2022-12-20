c3fb8f3298b23fd30659575231770702cb8595a2
Rob Campbell <raacampbell@users.noreply.github.com>
2022-12-20 11:14:03 +0000

    Switch to a more correct way of doing the laser fit but we still get issues with 0 mW

--- code/+zapit/@pointer/laser_mW_to_control.m
+++ code/+zapit/@pointer/laser_mW_to_control.m
@@ -14,11 +14,23 @@ if isempty(obj.laserFit)
     laserFit_mWToControl = fit(obj.laserMinMax_mW', obj.laserMinMaxControl','linear');
     controlVal = laserFit_mWToControl(mW);
 else
-    % Get a dumb conversion of sensor values to mW
-    sensorVals = [obj.laserFit.sensorValues(1);obj.laserFit.sensorValues(end)];
-    laserFit_mWToSensorVal = fit(obj.laserMinMax_mW', sensorVals,'linear');
-    sensorVal = laserFit_mWToSensorVal(mW);
+    % Re-scale the sensory values so they are in mW
+    mWvals = obj.laserFit.sensorValues;
+    mWvals = mWvals - min(mWvals);
+    mWvals = mWvals / max(mWvals);
 
-    controlVal = obj.laserFit.sensorOnControl(sensorVal);
+    % TODO -- for now let us just assume that it starts at zero
+    mWvals = mWvals * obj.laserMinMax_mW(2);
+
+    % We want the other way around, but this is the correct way to fit
+    laserFit_ControlToMW = fit(obj.laserFit.controlValues,mWvals,'poly5');
+
+    % So now we find the closest value
+    contV = linspace(obj.laserFit.controlValues(1),obj.laserFit.controlValues(end),100);
+    mwV = laserFit_ControlToMW(contV);
+
+    [~,ind] = min(abs(mwV-mW));
+
+    controlVal = contV(ind(1)); % take the first if there are multiple
 end
 
