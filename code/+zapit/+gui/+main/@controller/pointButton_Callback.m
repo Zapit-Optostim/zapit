a302c2c577ad5bc400b563d0a255c2cf50c46ba2
Rob Campbell <raacampbell@users.noreply.github.com>
2023-01-03 11:20:22 +0000

    pointer becomes a hand in interactive point mode

--- code/+zapit/+gui/+main/@controller/pointButton_Callback.m
+++ code/+zapit/+gui/+main/@controller/pointButton_Callback.m
@@ -3,12 +3,15 @@ function pointButton_Callback(obj,~,~)
 
     % Switch back and forth between click and point mode
     if obj.PointModeButton.Value == 1
-
+        % Entering point mode
         obj.CatMouseButton.Value = 0; % Both can not be activate at the the same time
 
         % Initially no last clicked position should be visible
         obj.hLastPoint.XData = nan;
         obj.hLastPoint.YData = nan;
+
+        % Pointer is a hand
+        obj.hFig.Pointer = 'hand';
 
         % Turn on laser
         obj.setCalibLaserSwitch('On');
@@ -16,6 +19,10 @@ function pointButton_Callback(obj,~,~)
         obj.hLastPoint.Visible = 'on';
 
     elseif obj.PointModeButton.Value == 0
+        % Leaving point mode
+
+        % Pointer is back to an arrow
+        obj.hFig.Pointer = 'arrow';
 
         % Turn off laser
         obj.setCalibLaserSwitch('Off');
