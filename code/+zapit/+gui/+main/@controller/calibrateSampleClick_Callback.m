ae400b8318e7303c87f2e3608dd79dda4daca6c6
Rob Campbell <git@raacampbell.com>
2023-01-13 11:20:45 +0000

    Move to larger methods out of main controller

---
+++ code/+zapit/+gui/+main/@controller/calibrateSampleClick_Callback.m
@@ -0,0 +1,49 @@
+function calibrateSampleClick_Callback(obj,sr,evt)
+    % Responds to mouse clicks during sample calibration
+    %
+    % zapit.gui.main.controller.calibrateSampleClick_Callback
+    %
+    % Purpose
+    % Responds to clicks for placing and translating/rotating the brain border
+    % during sample calibration. 
+    %
+    % Rob Campbell - SWC 2023
+
+    if strcmp(obj.hFig.SelectionType,'alt')
+        return
+    end
+    C = get (obj.hImAx, 'CurrentPoint');
+    X = C(1,1);
+    Y = C(1,2);
+
+    xl = obj.hImAx.XLim;
+    yl = obj.hImAx.YLim;
+    if X<xl(1) || X>xl(2) || Y<yl(1) || Y>yl(2)
+        return
+    end
+
+    % Paste
+    if obj.nInd == 1
+        % The boundaries of the brain in mm
+        b = obj.atlasData.whole_brain.boundaries_stereotax{1};
+
+        obj.plotOverlayHandles.brainOutline.XData = b(:,2)+X;
+        obj.plotOverlayHandles.brainOutline.YData = b(:,1)+Y;
+
+        obj.model.refPointsSample(obj.nInd,:) = [X,Y];
+        obj.nInd = obj.nInd + 1;
+        obj.plotOverlayHandles.bregma.XData=X;
+        obj.plotOverlayHandles.bregma.YData=Y;
+    elseif obj.nInd==2
+        if zapit.utils.isShiftPressed
+            obj.nInd = 1;
+            refPoints(:,2) = 0;
+            obj.plotOverlayHandles.bregma.XData=nan;
+            obj.plotOverlayHandles.bregma.YData=nan;
+        else
+            obj.nInd = obj.nInd + 1;
+        end %if isShiftPressed
+    end %if nInd
+
+
+end % calibrateSampleClick_Callback
---
+++ code/+zapit/+gui/+main/@controller/calibrateSampleRescaleOutline_Callback.m
@@ -0,0 +1,43 @@
+function calibrateSampleRescaleOutline_Callback(obj,~,evt)
+    % This callback scales and rotates the ARA outline as the mouse moves during sample calibration
+    %
+    % zapit.gui.main.controller.calibrateSampleRescaleOutline_Callback
+    %
+    % Purpose
+    % Responds to motion of the mouse over the axes during sample calibration. Allows the
+    % brain outline to be scaled and rotated around bregma.
+    %
+    % Rob Campbell - SWC 2023
+
+    C = get(obj.hImAx, 'CurrentPoint');
+    X = C(1,1);
+    Y = C(1,2);
+
+    xl = obj.hImAx.XLim;
+    yl = obj.hImAx.YLim;
+    if X<xl(1) || X>xl(2) || Y<yl(1) || Y>yl(2)
+        return
+    end
+    
+    % The boundaries of the brain in mm
+    b = obj.atlasData.whole_brain.boundaries_stereotax{1};
+
+    if obj.nInd==1
+        % follow mouse
+        obj.plotOverlayHandles.brainOutline.XData = b(:,2)+X;
+        obj.plotOverlayHandles.brainOutline.YData = b(:,1)+Y;
+    end
+
+    if obj.nInd==2
+        % Rotate and scale only if mouse cursor is over 1 mm from bregma
+        err = obj.model.refPointsSample(1,:)-[X,Y];
+        if sum(sqrt(err.^2))<1.5
+            return
+        end
+        obj.model.refPointsSample(obj.nInd,:) = [X,Y];
+        newpoint = zapit.utils.rotateAndScaleCoords(fliplr(b)', obj.model.refPointsStereotaxic, obj.model.refPointsSample)';
+        newpoint = fliplr(newpoint);
+        obj.plotOverlayHandles.brainOutline.XData = newpoint(:,2);
+        obj.plotOverlayHandles.brainOutline.YData = newpoint(:,1);
+    end
+end
