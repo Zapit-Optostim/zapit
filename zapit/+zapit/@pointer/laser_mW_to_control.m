1beb054ea33d0cdcbf88917e5e0858e2f1d87737
Rob Campbell <raacampbell@users.noreply.github.com>
2023-02-13 16:41:40 +0000

    Cap max laser control voltage so it can not exceed user settings.

--- zapit/+zapit/@pointer/laser_mW_to_control.m
+++ zapit/+zapit/@pointer/laser_mW_to_control.m
@@ -26,40 +26,52 @@ function controlVal = laser_mW_to_control(obj,mW)
     % zapit.pointer.generateLaserCalibrationCurve
 
 
-    % The laser fit is only needed if the laser is not linear. Since so far only very cheap
-    % lasers have been found to be non-linear we can not make a fuss if the fit is missing and
-    % just assume it is linear.
+
+    % Make local copies of setting needed in multiple places below
+    minPower = obj.settings.laser.laserMinMax_mW(1);
+    maxPower = obj.settings.laser.laserMinMax_mW(2);
+    maxCV = obj.settings.laser.laserMinMaxControlVolts(2);
+
     if isempty(obj.laserFit) || useLinear
-        minPower = obj.settings.laser.laserMinMax_mW(1);
-        maxPower = obj.settings.laser.laserMinMax_mW(2);
-        maxCV = obj.settings.laser.laserMinMaxControlVolts(2);
+        % The laser fit is only needed if the laser is not linear. Since so far only very cheap
+        % lasers have been found to be non-linear we can not make a fuss if the fit is missing and
+        % just assume it is linear.
+
         controlVal = (mW/maxPower) * maxCV - minPower ;
-        return
+    else
+        %Otherwise we handle the case where the user has a control curve
+
+        % Re-scale the sensor values so they are in mW
+        mWvals = obj.laserFit.sensorValues;
+        mWvals = mWvals - min(mWvals);
+        mWvals = mWvals / max(mWvals);
+
+        % TODO -- for now let us just assume that it starts at zero
+        mWvals = mWvals * maxPower;
+
+        % Make a plolynomial fit of the sensory values convert to mW as a function of the laser control value
+        % The is the correct way of doing the fit but it won't directly give us the answer we want, as we wish
+        % to know the control value that produces a given mW value.
+        laserFit_ControlToMW = fit(obj.laserFit.controlValues,mWvals,'poly3');
+
+        % Therefore we solve the problem by finding the closest value to our desired value
+        contV = linspace(obj.laserFit.controlValues(1),obj.laserFit.controlValues(end),10000);
+        mwV = laserFit_ControlToMW(contV);
+
+        % TODO -- need to interpolate. We currently get quantization if calibration was
+        % against the full scale.
+        [~,ind] = min(abs(mwV-mW));
+
+        controlVal = contV(ind(1)); % take the first if there are multiple
+
     end
 
-
-    % Re-scale the sensor values so they are in mW
-    mWvals = obj.laserFit.sensorValues;
-    mWvals = mWvals - min(mWvals);
-    mWvals = mWvals / max(mWvals);
-
-    % TODO -- for now let us just assume that it starts at zero
-    mWvals = mWvals * obj.settings.laser.laserMinMax_mW(2);
-
-    % Make a plolynomial fit of the sensory values convert to mW as a function of the laser control value
-    % The is the correct way of doing the fit but it won't directly give us the answer we want, as we wish
-    % to know the control value that produces a given mW value. 
-    laserFit_ControlToMW = fit(obj.laserFit.controlValues,mWvals,'poly3');
-
-    % Therefore we solve the problem by finding the closest value to our desired value
-    contV = linspace(obj.laserFit.controlValues(1),obj.laserFit.controlValues(end),10000);
-    mwV = laserFit_ControlToMW(contV);
-
-    % TODO -- need to interpolate. We currently get quantization if calibration was
-    % against the full scale.
-    [~,ind] = min(abs(mwV-mW));
-
-    controlVal = contV(ind(1)); % take the first if there are multiple
+    % Make sure the control value is not out of bounds
+    if controlVal>maxCV
+        fprintf('Requested value of %0.2f mW is out of range. Capping at laser max power of %0.2f mW\n',...
+            mW, maxPower)
+        controlVal = maxCV;
+    end
 
 
 end % laser_mW_to_control
